36620b1f2f72c0076271e1ff5bd2a3fed4cba050
hastyy <jp.mm.dias@gmail.com>
2025-11-19 21:25:34 +0000

    feat: add TimestampKeyGenerator

---
+++ internal/service/timestamp_key_generator.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/hastyy/murakami/internal/logtree"
+)
+
+type TimestampKeyGenerator func() logtree.Key
+
+func (gen TimestampKeyGenerator) GetNext() logtree.Key {
+	return gen()
+}
+
+func NewTimestampKeyGenerator() TimestampKeyGenerator {
+	return func() logtree.Key {
+		ts := time.Now().UnixMilli()
+		return logtree.Key(strconv.FormatInt(ts, 10))
+	}
+}
